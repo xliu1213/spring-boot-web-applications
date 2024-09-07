55f45c9fd33d85963c1142675355ed447520de4d
Xiyuan Liu <xliu1@tcd.ie>
2024-09-07 18:21:37 +0100

    Set up another Spring Boot project "namelist" with Spring Initializr

---
+++ namelist/src/main/java/ie/tcd/scss/namelist/NamelistApplication.java
@@ -0,0 +1,13 @@
+package ie.tcd.scss.namelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamelistApplication.class, args);
+	}
+
+}
---
+++ namelist/src/test/java/ie/tcd/scss/namelist/NamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.tcd.scss.namelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
